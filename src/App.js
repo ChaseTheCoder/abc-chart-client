1cebee415aaf90abb3b59ca3640e334faf99feca
Chase Sheaff <chasefsheaff@gmail.com>
2022-04-18 21:50:53 -0500

    chore: update name to initials

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Axios from 'axios';
 
 function App() {
   const [students, setStudents] = useState([]);
-  const [first, setFirst] = useState('')
-  const [last, setLast] = useState('')
+  const [initials, setInitials] = useState('');
+  // const [last, setLast] = useState('');
+
+  // const [abcList, setABCList] = useState([]);
+  // const [antecedents, setAntecedent] = useState('');
+  // const [behaviors, setBehavior] = useState('');
+  // const [consequences, setConsequence] = useState('');
 
   useEffect(() => {  // initial API request to database
     Axios.get('https://abc-chart.herokuapp.com/getStudents').then((response) => {
@@ -15,17 +20,38 @@ function App() {
 
   const createStudent = () => {
     Axios.post('https://abc-chart.herokuapp.com/createStudent', {
-      first, // first: first,-if two are same same just put it once
-      last //last: last  -makes it look better
+      initials, // first: first,-if two are same same just put it once
+      // last //last: last  -makes it look better
     }).then((response) => {
       setStudents([...students,
         {
-          first, // first: first,-if two are same same just put it once
-          last //last: last  -makes it look better
+          initials // first: first,-if two are same same just put it once
+          // last //last: last  -makes it look better
         }
       ])
     });
   };
+
+  // useEffect(() => {  // initial API request to database
+  //   Axios.get('localhost:3001/getABCList').then((response) => {
+  //     setABCList(response.data) // sets list of students to current data in API
+  //   })
+  // }, []);
+
+  // const createABCList = () => {
+  //   Axios.post('https://abc-chart.herokuapp.com/createABCList', {
+  //     first, // first: first,-if two are same same just put it once
+  //     last //last: last  -makes it look better
+  //   }).then((response) => {
+  //     setABCList([...abcList,
+  //       {
+  //         antecedents, // first: first,-if two are same same just put it once
+  //         behaviors, //last: last  -makes it look better
+  //         consequences
+  //       }
+  //     ])
+  //   });
+  // };
 
   return (
     <div className="App">
@@ -33,21 +59,34 @@ function App() {
         {students.map((student) => {
           return (
             <div>
-              <h1>First: {student.first}</h1>
-              <h1>Last: {student.last}</h1>
+              <h1>{student.initials}</h1>
             </div>
           );
         })}
       </div>
       <div>
-        <input type="text" placeholder='first initial' onChangeCapture={(event) => {
-          setFirst(event.target.value);
+        <input type="text" placeholder='initials of student' onChangeCapture={(event) => {
+          setInitials(event.target.value);
         }}/>
-        <input type="text" placeholder='last initial' onChangeCapture={(event) => {
+        {/* <input type="text" placeholder='last initial' onChangeCapture={(event) => {
           setLast(event.target.value);
-        }}/>
+        }}/> */}
         <button onClick={createStudent}>Create Student</button>
       </div>
+      {/* <div>
+        {Object.keys(abcList).map((key) => {
+         return (
+           <div key={key}>
+              <h1>{key}</h1>
+              {abcList[key].map((behaviors) => {
+                return (
+                 <span key={behaviors.id}>{behaviors.title}</span>
+                )
+               })}
+           </div>
+         )
+       })}
+     </div> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById('root'));root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
